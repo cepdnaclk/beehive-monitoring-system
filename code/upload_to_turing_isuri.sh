8dde372f36f69ec25b2d776382e23a5652c093e3
ridmajayasundara <e18155@eng.pdn.ac.lk>
2024-12-21 08:48:23 +0530

    server upload scipt added

---
+++ code/upload_to_turing_isuri.sh
@@ -0,0 +1,7 @@
+# upload the code files to turing server
+rsync -avz \
+  --exclude='code/datasets/mobile-videos/*' \
+  --exclude='**/*.pyc' \
+  -e "ssh -J e17058@aiken.ce.pdn.ac.lk" * \
+  e17058@turing.ce.pdn.ac.lk:/storage/projects3/ridma-beehive
+
