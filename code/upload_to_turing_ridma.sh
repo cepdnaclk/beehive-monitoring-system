6064593b10a51e9b26b3faf353b0ba5620710559
ridmajayasundara <e18155@eng.pdn.ac.lk>
2024-12-21 15:03:17 +0530

    codebase manament scripts modified and added

--- code/upload_to_turing_ridma.sh
+++ code/upload_to_turing_ridma.sh
@@ -1,6 +1,7 @@
 # upload the code files to turing server
 rsync -avz \
   --exclude='code/datasets/mobile-videos/*' \
+  --exclude='code/datasets/pre-processed-mobile-videos/*' \
   --exclude='**/*.pyc' \
   -e "ssh -J e18155@aiken.ce.pdn.ac.lk" * \
   e18155@turing.ce.pdn.ac.lk:/storage/projects3/ridma-beehive
